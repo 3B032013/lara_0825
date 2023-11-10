e21336ca5e7ad4a467897eca68fb60a5f6472d9a
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2023-11-10 09:51:37 +0800

    了解Model物件和Collection的差異

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,18 @@ Route::get('/', function () {
     // $post = Post::find(1);
     // $post->delete();
 
-    Post::destroy(2);
-    Post::destroy(3,5,7);
+    // Post::destroy(2);
+    // Post::destroy(3,5,7);
+
+    // $allPosts = Post::all();
+    // dd($allPosts);
+
+    // $featuredPosts = Post::where('is_feature',1)->get();
+    // dd($featuredPosts);
+
+    // $fourthPost = Post::find(4);
+    // dd($fourthPost);
+
+    $lastPost = Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 });
